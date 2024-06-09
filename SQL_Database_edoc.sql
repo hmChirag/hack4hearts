c885ce3c8fef7ef8d559c9d290a5a9eee9ee40e6
Chirag H M <149199306+hmChirag@users.noreply.github.com>
2024-06-09 10:07:04 +0530

    Update SQL_Database_edoc.sql

--- SQL_Database_edoc.sql
+++ SQL_Database_edoc.sql
@@ -3,7 +3,6 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 19, 2022 at 01:39 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.5
 
@@ -11,13 +10,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `edoc`
 --
@@ -163,31 +155,12 @@ INSERT INTO `schedule` (`scheduleid`, `docid`, `title`, `scheduledate`, `schedul
 DROP TABLE IF EXISTS `specialties`;
 
 
-
-
-
-
-
-
-
-
 CREATE TABLE prescription (
     id INT AUTO_INCREMENT PRIMARY KEY,
     patient_email VARCHAR(255) NOT NULL,
     prescription_content TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -282,7 +255,3 @@ INSERT INTO `webuser` (`email`, `usertype`) VALUES
 ('patient@edoc.com', 'p'),
 ('emhashenudara@gmail.com', 'p');
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
